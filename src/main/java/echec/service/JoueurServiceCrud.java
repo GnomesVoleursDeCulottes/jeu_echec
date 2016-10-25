4f92468fb51638a1fa19dd73a99b979cb907cae0
Nordine59 <itounour@hotmail.fr>
2016-10-25 16:11:31 +0200

    JoueurCrudService : création du service

---
+++ src/main/java/echec/service/JoueurServiceCrud.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package echec.service;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author admin
+ */
+public interface JoueurServiceCrud extends CrudRepository<Joueur, Long>{
+    
+    
+    
+     public Joueur findOneByLogin(String login);
+     
+      @Override
+    public List<Joueur> findAll();
+    
+    
+    
+}
