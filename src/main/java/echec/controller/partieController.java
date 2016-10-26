36d8ea70a908900651bab4e1b707b53a44fe8df9
fantay <laurentcoll@hotmail.com>
2016-10-26 11:38:06 +0200

    partieController.java ajout des Autowired serviceCRUD et de la route listePartie avec envoie des info des partie existantes

--- src/main/java/echec/controller/partieController.java
+++ src/main/java/echec/controller/partieController.java
@@ -5,10 +5,35 @@
  */
 package echec.controller;
 
+import echec.service.JoueurServiceCrud;
+import echec.service.PartieServiceCrud;
+import echec.service.pionServiceCrud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  *
  * @author Laurent-LIM
  */
+@Controller
 public class partieController {
     
+    @Autowired
+    private JoueurServiceCrud serviceJoueur;
+    
+    @Autowired
+    private PartieServiceCrud servicePartie;
+    
+    @Autowired
+    private pionServiceCrud servicePion;
+    
+    @RequestMapping(value = "/listePartie", method = RequestMethod.GET)
+    public String listePartie(Model model){
+        model.addAttribute("listePartie", servicePartie.findAll());
+        return "_listePartie.jsp";
+    }
 }
