5abe2b9079a6d4637ff48a5eba55750ca75ac0ec
Nordine59 <itounour@hotmail.fr>
2016-10-26 10:25:43 +0200

    EntityJoueur : création des relations avec la table partie

--- src/main/java/echec/entity/Joueur.java
+++ src/main/java/echec/entity/Joueur.java
@@ -12,6 +12,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 /**
  *
@@ -29,6 +30,8 @@ public class Joueur implements Serializable {
     
     private String mdp;
     
+    
+    @OneToOne(mappedBy = "leJoueur")
     private List<Partie> parties = new ArrayList<Partie>();
 
     public String getPseudo() {
