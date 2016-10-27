d91203233efbbb63f76f6e3060c31a2e736ae965
fantay <laurentcoll@hotmail.com>
2016-10-27 14:32:53 +0200

    PieceServiceCrud.java refactor de PionServiceCrud.java

---
+++ src/main/java/echec/service/PieceServiceCrud.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package echec.service;
+
+import echec.entity.Piece;
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author admin
+ */
+public interface PieceServiceCrud extends CrudRepository<Piece, Long>{ 
+    
+    
+    @Override
+    public List<Piece> findAll();
+    
+    
+}
