2ea59f3ccdfb6c530ad525f42499fee17ec423e5
tmpUsername <julien.duthoit.dev@gmail.com>
2016-10-27 16:12:01 +0200

    jeu.js: ajout du css pour le tableau

--- src/main/webapp/JS/jeu.js
+++ src/main/webapp/JS/jeu.js
@@ -2,15 +2,22 @@
  * Contient les différentes fonctions liées au jeu
  */
 
+$(document).ready(function () {
+    $("tr:even td:odd").css("background-color", "black");
+    $("tr:even td:even").css("background-color", "white");
+    $("tr:odd td:even").css("background-color", "black");
+    $("tr:odd td:odd").css("background-color", "white");
+});
+
 /**/
-var estLibre = function(){
+var estLibre = function () {
     return true;
 };
 
-var estPrenable = function() {
+var estPrenable = function () {
     return true;
 };
 
-var deplacementPion = function (){
-    
+var deplacementPion = function () {
+
 };
