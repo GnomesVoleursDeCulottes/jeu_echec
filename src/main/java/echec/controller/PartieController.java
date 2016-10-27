f46c0735e07d487a2eeb11351f10efbd2aa010bf
fantay <laurentcoll@hotmail.com>
2016-10-27 11:52:58 +0200

    JoueurController.java modification d'un commentaire

--- src/main/java/echec/controller/PartieController.java
+++ src/main/java/echec/controller/PartieController.java
@@ -16,6 +16,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import echec.service.PionServiceCrud;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 
 /**
@@ -24,36 +25,43 @@ import org.springframework.web.bind.annotation.PathVariable;
  */
 @Controller
 public class PartieController {
-    
+
     @Autowired
     private JoueurServiceCrud serviceJoueur;
-    
+
     @Autowired
     private PartieServiceCrud servicePartie;
-    
+
     @Autowired
     private PionServiceCrud servicePion;
-    
+
+    //fonction qui affiche la liste de toutes les parties
     @RequestMapping(value = "/lister_parties", method = RequestMethod.GET)
-    public String listePartie(Model model){
+    public String listePartie(Model model) {
         model.addAttribute("listePartie", servicePartie.findAll());
         return "parties.jsp";
     }
-    
-    @RequestMapping(value = "/partie_en_cours",method = RequestMethod.GET)
-    public String partieEnCours(Model model, HttpSession s){
+
+    //fonction qui affiche la listes des parties d'un joueur donné
+    @RequestMapping(value = "/partie_en_cours", method = RequestMethod.GET)
+    public String partieEnCours(Model model, HttpSession s) {
         Long joueur = (long) s.getAttribute("idUser");
         model.addAttribute("listePartie", servicePartie.findAllByBlancId(joueur));
         return "_PARTIE_EN_COURS.jsp";
     }
-    
-    @RequestMapping(value = "/creePartie/{nomPartie}", method = RequestMethod.POST)
-    public String creePartie(Model model, HttpSession s, @PathVariable("nomPartie")String nomPartie){
-        Long joueur = (long) s.getAttribute("idUser");
-        Partie partie = new Partie();
-        partie.setNomPartie(nomPartie);
-        
+
+    //fonction qui affiche le formulaire de création d'une partie
+    @RequestMapping(value = "/cree_partie", method = RequestMethod.GET)
+    public String creePartie(Model model, HttpSession s){
         return "creer_partie.jsp";
     }
     
+    //fonction de création d'une partie par validation du formulaire de la page _CREATION_PARTIE.jsps
+    @RequestMapping(value = "/cree_partie", method = RequestMethod.POST)
+    public String creePartie(@ModelAttribute ("PartieAttribut") Partie partie, HttpSession s) {
+        Long joueur = (long) s.getAttribute("idUser");
+        servicePartie.save(partie);
+        return "redirect:/creer_partie.jsp";
+    }
+
 }
